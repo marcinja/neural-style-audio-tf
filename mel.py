11a2b931eb36ab3d1a3181c7a65950763491839f
Marcin Jachymiak <marcinja@mit.edu>
2017-12-11 03:37:48 -0500

    start writing mel net

--- mel.py
+++ mel.py
@@ -45,23 +45,25 @@ def mel_spec(S):
     return librosa.feature.melspectrogram(S=S)
 
 
-content_spec, fs = read_audio_spectum(CONTENT_FILENAME)
-style_spec, fs = read_audio_spectum(STYLE_FILENAME)
+a_content, fs = read_audio_spectum(CONTENT_FILENAME)
+a_style, fs = read_audio_spectum(STYLE_FILENAME)
 
-mel_content = mel_spec(content_spec)
-mel_style = mel_spec(style_spec)
+mel_content = mel_spec(a_content)
+mel_style = mel_spec(a_style)
 
-'''
 N_SAMPLES = a_content.shape[1]
 N_CHANNELS = a_content.shape[0]
 a_style = a_style[:N_CHANNELS, :N_SAMPLES]
 
 
+N_SAMPLES_MEL = mel_content.shape[1]
+N_CHANNELS_MEL = mel_content.shape[0]
+mel_style = mel_style[:N_CHANNELS_MEL, :N_SAMPLES_MEL]
 # ### Visualize spectrograms for content and style tracks
 
 # In[ ]:
 
-
+"""
 plt.figure(figsize=(10, 5))
 plt.subplot(1, 2, 1)
 plt.title('Content')
@@ -70,27 +72,41 @@ plt.subplot(1, 2, 2)
 plt.title('Style')
 plt.imshow(a_style[:400,:])
 plt.show()
-
+"""
 
 # ### Compute content and style feats
 
-# In[ ]:
-
-
 N_FILTERS = 4096
+N_FILTERS_MEL = 1025
+
+FILTER_WIDTH = 11
+MEL_FILTER_WIDTH = 50
 
 a_content_tf = np.ascontiguousarray(a_content.T[None,None,:,:])
 a_style_tf = np.ascontiguousarray(a_style.T[None,None,:,:])
 
+mel_content_tf = np.ascontiguousarray(mel_content.T[None,None,:,:])
+mel_style_tf = np.ascontiguousarray(mel_style.T[None,None,:,:])
+
 # filter shape is "[filter_height, filter_width, in_channels, out_channels]"
 std = np.sqrt(2) * np.sqrt(2.0 / ((N_CHANNELS + N_FILTERS) * 11))
-kernel = np.random.randn(1, 11, N_CHANNELS, N_FILTERS)*std
-    
+kernel = np.random.randn(1, FILTER_WIDTH, N_CHANNELS, N_FILTERS)*std
+
+std_mel = np.sqrt(2) * np.sqrt(2.0 / ((N_CHANNELS_MEL + N_FILTERS_MEL) * 11))
+kernel_mel = np.random.randn(1, MEL_FILTER_WIDTH, N_CHANNELS_MEL, N_FILTERS_MEL)*std
+
+
 g = tf.Graph()
 with g.as_default(), g.device('/cpu:0'), tf.Session() as sess:
     # data shape is "[batch, in_height, in_width, in_channels]",
     x = tf.placeholder('float32', [1,1,N_SAMPLES,N_CHANNELS], name="x")
 
+    # convert back to 2d array
+    x_as_np = np.squeeze(x)
+    mel_x = mel_spec(x)
+    mel_x = np.ascontiguousarray(mel_x.T[None,None,:,:])
+
+    # STFT Net
     kernel_tf = tf.constant(kernel, name="kernel", dtype='float32')
     conv = tf.nn.conv2d(
         x,
@@ -98,8 +114,21 @@ with g.as_default(), g.device('/cpu:0'), tf.Session() as sess:
         strides=[1, 1, 1, 1],
         padding="VALID",
         name="conv")
-    
+
     net = tf.nn.relu(conv)
+
+    # now do mel net
+    kernel_tf_mel = tf.constant(kernel_mel, name="kernel_mel", dtype='float32')
+    conv_mel = tf.nn.conv2d(
+        mel_x,
+        kernel_tf_mel,
+        strides=[1, 1, 1, 1],
+        padding="VALID",
+        name="conv_mel")
+
+    mel_net = tf.nn.relu(conv_mel)
+
+
 
     content_features = net.eval(feed_dict={x: a_content_tf})
     style_features = net.eval(feed_dict={x: a_style_tf})
@@ -216,4 +245,3 @@ plt.show()
 plt.show()
 
 
-'''
